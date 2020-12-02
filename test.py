e04b55b8911eca1a5e297d6f17b6b340c39a136d
Kevin <jinc0008@e.ntu.edu.sg>
2020-12-02 13:53:09 +0800

    test

--- test.py
+++ test.py
@@ -30,8 +30,9 @@ import os
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
-from util.visualizer import save_images
-from util import html
+import util
+import ntpath
+from PIL import Image
 
 
 if __name__ == '__main__':
@@ -45,12 +46,15 @@ if __name__ == '__main__':
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
+    '''
     # create a website
     web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
     if opt.load_iter > 0:  # load_iter is 0 by default
         web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
     print('creating web directory', web_dir)
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+    '''
+
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
     # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
@@ -65,5 +69,18 @@ if __name__ == '__main__':
         img_path = model.get_image_paths()     # get image paths
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-    webpage.save()  # save the HTML
+        
+        res_dir = os.path.join('./result',opt.name)
+        util.mkdirs(res_dir)
+        name = os.path.splitext(ntpath.basename(img_path[0]))[0]
+        for label,im_data in visuals.items():
+            im = util.tensor2im(im_data) 
+            img_name = '%s_%s.png' % (name,label)
+            save_path = os.path.join(res_dir,img_name)
+
+            im_pil = Image.fromarray(im).resize((960,544),Image.BICUBIC)
+            im_pil.save(save_path)
+
+
+        # save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+    # webpage.save()  # save the HTML
